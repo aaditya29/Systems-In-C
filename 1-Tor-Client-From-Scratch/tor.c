cd97fcc1a193ec95a3984a7353a3f95ae2b0cac2
Aditya Mishra <mishraaaditya29@gmail.com>
2024-11-30 09:19:35 +0530

    Added command-line arguments for host and port in tor.c and include necessary headers in tor.h

--- 1-Tor-Client-From-Scratch/tor.c
+++ 1-Tor-Client-From-Scratch/tor.c
@@ -2,4 +2,20 @@
 
 int main(int argc, char *argv[])
 {
+    char *host; // host is server we wanna connnect to
+    int port;   // for port number
+
+    if (argc < 3)
+    {
+        /*
+        ./tor is argv[0]
+        1.2.3.4 is the host
+        80 is the port
+        */
+        printf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+        return -1; // for the error
+    }
+
+    host = argv[1];       // pointing host at argv[1]
+    port = atoi(argv[2]); // pointing port at argv[2] using atoi for the string
 }
--- 1-Tor-Client-From-Scratch/tor.h
+++ 1-Tor-Client-From-Scratch/tor.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #define PROXY "127.0.01" // IP Address of local host
 #define PROXYPORT 9050   // default proxy port
