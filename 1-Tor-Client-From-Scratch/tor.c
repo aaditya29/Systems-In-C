e018850bc32ba6fe9b867d7c893b1ed7bc97a4cd
Aditya Mishra <mishraaaditya29@gmail.com>
2024-11-30 09:56:58 +0530

    Fixed printf to fprintf for error message in tor.c

--- 1-Tor-Client-From-Scratch/tor.c
+++ 1-Tor-Client-From-Scratch/tor.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
         1.2.3.4 is the host
         80 is the port
         */
-        printf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
         return -1; // for the error
     }
 
