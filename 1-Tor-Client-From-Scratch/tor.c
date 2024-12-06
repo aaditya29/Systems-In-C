714e6db1f65fa3905c5cf6f11e6214e97e09330f
Aditya Mishra <mishraaaditya29@gmail.com>
2024-12-06 11:22:35 +0530

    Added response handling and success check in main function for proxy connection

--- 1-Tor-Client-From-Scratch/tor.c
+++ 1-Tor-Client-From-Scratch/tor.c
@@ -26,7 +26,9 @@ int main(int argc, char *argv[])
     int s;
     struct sockaddr_in sock; // struct to hold ip addresses
     Req *req;
+    Res *res;
     char buf[ressize];
+    int success; // making predicate or boolean operation for true or false
 
     if (argc < 3)
     {
@@ -53,13 +55,17 @@ int main(int argc, char *argv[])
     /*
     Below we areconfiguring a `sockaddr_in` structure which is used to specify an address to which to connect a socket.
 
-    sock.sin_family = AF_INET; //`sock.sin_family` is being set to `AF_INET`, which indicates that the address family is IPv4. This is necessary for the socket to understand that it will be communicating over an IPv4 network.
+    sock.sin_family = AF_INET; //`sock.sin_family` is being set to `AF_INET`, which indicates that the address family is IPv4.
+    This is necessary for the socket to understand that it will be communicating over an IPv4 network.
 
-    sock.sin_port = htons(PROXYPORT);//`sock.sin_port` is being set to the port number, which is converted to network byte order using the `htons` function. `htons` stands for "host to network short" and ensures that the port number is in the correct byte order for network communication.
+    sock.sin_port = htons(PROXYPORT);//`sock.sin_port` is being set to the port number, which is converted to network byte order using the `htons` function.
+    `htons` stands for "host to network short" and ensures that the port number is in the correct byte order for network communication.
 
-    sock.sin_addr.s_addr = inet_addr(PROXY);// `sock.sin_addr.s_addr` is being set to the IP address, which is converted from a string format to a network address using the `inet_addr` function. `PROXY` is expected to be a string containing the IP address (e.g., "192.168.1.1").
+    sock.sin_addr.s_addr = inet_addr(PROXY);// `sock.sin_addr.s_addr` is being set to the IP address, which is converted from a string format to a network address using the `inet_addr` function.
+    `PROXY` is expected to be a string containing the IP address (e.g., "192.168.1.1").
 
-    this code is preparing a `sockaddr_in` structure with the necessary details (address family, port number, and IP address) to establish a network connection using a socket.
+    this code is preparing a `sockaddr_in` structure with the necessary details (address family, port number, and IP address) to
+    establish a network connection using a socket.
     */
 
     sock.sin_family = AF_INET;               // setting the address family to IPv4
@@ -88,6 +94,21 @@ int main(int argc, char *argv[])
 
         return -1;
     }
-    close(s); // closing the socket
+    res = buf;                 // to access proxy response
+    success = (res->cd == 90); // if cd == 90 then success is true else false
+    if (!success)
+    {
+        fprintf(stderr, "Unable to establish the connection and traverse the proxy."
+                        "Error code: %d\n",
+                res->cd);
+        close(s);
+        free(req);
+
+        return -1;
+    }
+
+    printf("Successfully connected through PROXY to %s:%d\n", host, port);
+    close(s);  // closing the socket
+    free(req); // freeing the memory
     return 0;
 }
