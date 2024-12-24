d88804720334b921b57c95f310e3b1ef10aa8f63
Aditya Mishra <mishraaaditya29@gmail.com>
2024-12-24 11:42:54 +0530

    Added s_leaf structure

--- 3-Database-Server/tree/tree.h
+++ 3-Database-Server/tree/tree.h
@@ -29,9 +29,19 @@ typedef unsigned short int int16;
 typedef unsigned char int8;
 struct s_node
 {
-    struct u_tree *north; // for pointing upwards and if on top will point to itself
+    struct s_node *north; // for pointing upwards and if on top will point to itself
     struct s_node *west;  // for pointing to the left
     struct s_leaf *east;  // for pointing to the right
     int8 path[256];
 };
 typedef struct s_node Node;
+
+struct s_leaf
+{
+    union u_tree *west;  // for pointing to the left
+    struct s_leaf *east; // for pointing to the right
+    int8 key[128];       // for storing the key
+    int8 *value;         // for storing the value
+    int16 size;          // for storing the size of the value
+};
+typedef struct s_leaf Leaf;
