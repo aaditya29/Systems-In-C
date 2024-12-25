b838772d1bd3b00f4912da7f2f4e0cb16efc985f
Aditya Mishra <mishraaaditya29@gmail.com>
2024-12-25 22:22:44 +0530

    Implemented main function to initialize tree structure and display root path

--- 3-Database-Server/tree/tree.c
+++ 3-Database-Server/tree/tree.c
@@ -1,1 +1,11 @@
 #include "tree.h"
+int main()
+{
+    Tree *root = (Tree *)malloc(sizeof(Tree));
+    root->n.north = &root->n;
+    root->n.west = NULL;
+    root->n.east = NULL;
+    strcpy(root->n.path, "Root");
+    printf("Root: %s\n", root->n.path);
+    return 0;
+}
