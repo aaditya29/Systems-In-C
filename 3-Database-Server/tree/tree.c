94a80227eea533dc3b6b19e8cf8e87802c949580
Aditya Mishra <mishraaaditya29@gmail.com>
2024-12-26 15:41:43 +0530

    Refactored root tree initialization

--- 3-Database-Server/tree/tree.c
+++ 3-Database-Server/tree/tree.c
@@ -1,16 +1,16 @@
 #include "tree.h"
 
 // defining the root
-Tree root = {
-    .tag = (TagRoot | TagNode), // either root or node
-    .north = (Tree *)&root,     // if points to itself then it is root
-    .west = 0,                  // initialise to 0
-    .east = 0,
-    .path = "/",
-};
+Tree root = {.n = {
+                 .tag = (TagRoot | TagNode), // either root or node
+                 .north = (Node *)&root,     // if points to itself then it is root
+                 .west = 0,                  // initialise to 0
+                 .east = 0,
+                 .path = "/",
+             }};
 
 int main()
 {
-    printf("%p\n", &root);
+    printf("%p\n", (void *)&root);
     return 0;
 }
