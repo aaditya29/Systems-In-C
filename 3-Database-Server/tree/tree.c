8d42d74c645364595c23e7f3178551f3e74ac399
Aditya Mishra <mishraaaditya29@gmail.com>
2024-12-26 15:37:19 +0530

    Defined root tree structure

--- 3-Database-Server/tree/tree.c
+++ 3-Database-Server/tree/tree.c
@@ -1,1 +1,16 @@
 #include "tree.h"
+
+// defining the root
+Tree root = {
+    .tag = (TagRoot | TagNode), // either root or node
+    .north = (Tree *)&root,     // if points to itself then it is root
+    .west = 0,                  // initialise to 0
+    .east = 0,
+    .path = "/",
+};
+
+int main()
+{
+    printf("%p\n", &root);
+    return 0;
+}
