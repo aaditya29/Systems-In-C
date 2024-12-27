53010f3899bafc18d996c0d3bfb232b34616e1ee
Aditya Mishra <mishraaaditya29@gmail.com>
2024-12-27 15:02:31 +0530

    added comment

--- 3-Database-Server/tree/tree.c
+++ 3-Database-Server/tree/tree.c
@@ -5,8 +5,8 @@ Tree root = {.n = {
                  .tag = (TagRoot | TagNode), // either root or node
                  .north = (Node *)&root,     // if points to itself then it is root
                  .west = 0,                  // initialise to 0
-                 .east = 0,
-                 .path = "/",
+                 .east = 0,                  // initialise to 0
+                 .path = "/",                // root path
              }};
 
 int main()
