5f9d9156a35a556e45a87d4b5e7251d140ca36e9
Aditya Mishra <mishraaaditya29@gmail.com>
2025-01-07 10:37:59 +0530

    defined port

--- 3-Database-Server/cache22/cache22.h
+++ 3-Database-Server/cache22/cache22.h
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+#define HOST "127.0.0.1"
+#define PORT 12000
+
 typedef unsigned int int32;       // defining the integer of 32 bits
 typedef unsigned short int int16; // defining the integer of 16 bits
 typedef unsigned char int8;       // defining the integer of 8 bits
