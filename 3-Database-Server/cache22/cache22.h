7978d5c827953ad1cba26e4cb80a3cde0836fc1d
Aditya Mishra <mishraaaditya29@gmail.com>
2025-01-07 10:40:43 +0530

    added header files

--- 3-Database-Server/cache22/cache22.h
+++ 3-Database-Server/cache22/cache22.h
@@ -1,8 +1,12 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+
+#include <assert.h>
+#include <errno.h>
 
 #define HOST "127.0.0.1"
 #define PORT 12000
