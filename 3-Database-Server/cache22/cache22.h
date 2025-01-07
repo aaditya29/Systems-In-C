9a9a3894269ad888cc21bd7b0ee3ead9e698f08c
Aditya Mishra <mishraaaditya29@gmail.com>
2025-01-07 10:23:11 +0530

    added header files

--- 3-Database-Server/cache22/cache22.h
+++ 3-Database-Server/cache22/cache22.h
@@ -3,3 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+
+typedef unsigned int int32;       // defining the integer of 32 bits
+typedef unsigned short int int16; // defining the integer of 16 bits
+typedef unsigned char int8;       // defining the integer of 8 bits
+
+void mainloop(void);
+void main(int, char **);
