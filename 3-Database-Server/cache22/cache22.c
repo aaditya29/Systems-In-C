08f6a1722d457f5afbf52d58edd1a806e19a5c4b
Aditya Mishra <mishraaaditya29@gmail.com>
2025-01-07 10:57:49 +0530

    added server socket binding

--- 3-Database-Server/cache22/cache22.c
+++ 3-Database-Server/cache22/cache22.c
@@ -2,6 +2,26 @@
 
 bool scontinuation; // global variable for the server continuation
 
+void mainloop(void)
+{
+    struct sockaddr_in sock;
+    int s;
+
+    sock.sin_family = AF_INET;              // setting the family of the socket
+    sock.sin_port = htons(PORT);            // setting the port of the socket
+    sock.sin_addr.s_addr = inet_addr(HOST); // setting the address of the socket
+
+    s = socket(AF_INET, SOCK_STREAM, 0); // creating a socket
+    assert(s > 0);
+
+    // binding the socket with the structure
+    if (bind(s, (struct sockaddr *)&sock, sizeof(sock)))
+    {
+        assert_perror(errno); // checking if there is an error
+    }
+    listen(s, 20); // listening to the socket making 20 connection
+}
+
 int main(int argc, char *argv[])
 {
     scontinuation = true;
